19a502c28282a05827fc99d42b71d679dcc8cf8d
Huwa-hank <hankshen1@gmail.com>
2025-10-15 22:38:44 +0800

    Delete myproject directory

--- myproject/app.py
+++
@@ -1,8 +0,0 @@
-# Example route returning structured JSON data
-@app.route("/api/data")
-def get_data():
-    return jsonify({
-        "status": "ok",
-        "message": "Package test successful",
-        "data": [1, 2, 3, 4, 5]
-    })
